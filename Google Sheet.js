79154fd257b02fd4d48970022f1e5f98fd6c5853
blackhole <bimaardiantopramana@gmail.com>
2024-06-26 09:01:08 +0700

    asdasd

--- Google Sheet.js
+++ Google Sheet.js
@@ -5,7 +5,21 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+    .then(response => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Thank you!',
+        text: 'Your form has been submitted successfully.',
+      }).then(() => {
+        window.location.reload();
+      });
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong! Please try again.',
+      });
+    });
+});
